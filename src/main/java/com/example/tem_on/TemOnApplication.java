0d4cac70d68993402a8aab9e6c49436224c3236d
Lee Gyungmin <rudals831@gmail.com>
2026-05-21 22:41:32 +0900

    Init Spring Boot project

---
+++ src/main/java/com/example/tem_on/TemOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.tem_on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tem_on/TemOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tem_on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
