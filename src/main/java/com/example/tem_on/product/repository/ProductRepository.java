f0582276b347bccc40d02d90351535ab3987767d
Lee Gyungmin <rudals831@gmail.com>
2026-05-26 22:29:26 +0900

    fix: 상품 repo 카테고리 필터 기능 추가

--- src/main/java/com/example/tem_on/product/repository/ProductRepository.java
+++ src/main/java/com/example/tem_on/product/repository/ProductRepository.java
@@ -1,7 +1,25 @@
 package com.example.tem_on.product.repository;
 
 import com.example.tem_on.product.domain.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryAndNameContaining(
+            String category,
+            String keyword,
+            Pageable pageable
+    );
+
+    Page<Product> findByCategory(
+            String category,
+            Pageable pageable
+    );
+
+    Page<Product> findByNameContaining(
+            String keyword,
+            Pageable pageable
+    );
 }
