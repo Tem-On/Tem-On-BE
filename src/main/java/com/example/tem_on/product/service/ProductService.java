916e5f9e695e2a91010257c6f80515f2dd088960
Lee Gyungmin <rudals831@gmail.com>
2026-05-26 22:31:06 +0900

    fix: ProductService 페이지네이션, 카테고리 필터링 기능

--- src/main/java/com/example/tem_on/product/service/ProductService.java
+++ src/main/java/com/example/tem_on/product/service/ProductService.java
@@ -4,9 +4,9 @@ import com.example.tem_on.product.domain.dto.ProductResponse;
 import com.example.tem_on.product.domain.entity.Product;
 import com.example.tem_on.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,15 +14,39 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    // 상품 목록 조회
-    public List<ProductResponse> findAllProducts() {
-        return productRepository.findAll()
-                .stream()
-                .map(ProductResponse::from)
-                .toList();
+    public Page<ProductResponse> findProducts(
+            String category,
+            String keyword,
+            Pageable pageable
+    ) {
+        Page<Product> products;
+
+        boolean hasCategory = category != null && !category.isBlank();
+        boolean hasKeyword = keyword != null && !keyword.isBlank();
+
+        if (hasCategory && hasKeyword) {
+            products = productRepository.findByCategoryAndNameContaining(
+                    category,
+                    keyword,
+                    pageable
+            );
+        } else if (hasCategory) {
+            products = productRepository.findByCategory(
+                    category,
+                    pageable
+            );
+        } else if (hasKeyword) {
+            products = productRepository.findByNameContaining(
+                    keyword,
+                    pageable
+            );
+        } else {
+            products = productRepository.findAll(pageable);
+        }
+
+        return products.map(ProductResponse::from);
     }
 
-    // 상품 상세 조회
     public ProductResponse findProduct(Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
