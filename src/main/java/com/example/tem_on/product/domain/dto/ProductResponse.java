10a9f4e29553e1a3cdf1071bb90ac8180179ae11
Lee Gyungmin <rudals831@gmail.com>
2026-05-26 22:13:40 +0900

    feat: ProductResponse 설계

---
+++ src/main/java/com/example/tem_on/product/domain/dto/ProductResponse.java
@@ -0,0 +1,36 @@
+package com.example.tem_on.product.domain.dto;
+
+import com.example.tem_on.product.domain.entity.Product;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ProductResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+    private String imageUrl;
+    private String category;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static ProductResponse from(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .imageUrl(product.getImageUrl())
+                .category(product.getCategory())
+                .status(product.getStatus())
+                .createdAt(product.getCreatedAt())
+                .updatedAt(product.getUpdatedAt())
+                .build();
+    }
+}
