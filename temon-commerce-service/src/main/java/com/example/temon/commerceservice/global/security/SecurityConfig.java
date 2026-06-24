793cad0bf070d29f7155ef4e242f300bfefc5384
M4rs0312 <jhs9221@gmail.com>
2026-06-24 15:04:04 +0900

    feat: commerce-service 분리

---
+++ temon-commerce-service/src/main/java/com/example/temon/commerceservice/global/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.temon.commerceservice.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/auth/oauth/kakao",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                // 아래는 테스트용으로 열어둔것들
+                                "/ws/**",
+                                "/ws-test.html",
+                                "/queue-ws-test.html",
+                                "/admin-dashboard-test.html",
+                                "/event-ws-test.html",
+                                "/monitoring-ws-test.html",
+                                "/admin-order-test.html",
+                                "/api/products/**", 
+                                "/api/events/**",
+                                "/api/admin/**"
+                        ).permitAll()
+
+                        .requestMatchers("/api/admin/**")
+                        .hasRole("ADMIN")
+
+                        .anyRequest()
+                        .authenticated()
+                );
+
+        return http.build();
+    }
+}
