c81d40cdad2a9414c9ea0e4947d256a3e62f1daf
M4rs0312 <jhs9221@gmail.com>
2026-06-30 23:34:47 +0900

    fix: commerceapllication 수정

--- temon-commerce-service/src/main/java/com/example/temon/commerceservice/TemonCommerceServiceApplication.java
+++ temon-commerce-service/src/main/java/com/example/temon/commerceservice/TemonCommerceServiceApplication.java
@@ -2,7 +2,9 @@ package com.example.temon.commerceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TemonCommerceServiceApplication {
     public static void main(String[] args) {
